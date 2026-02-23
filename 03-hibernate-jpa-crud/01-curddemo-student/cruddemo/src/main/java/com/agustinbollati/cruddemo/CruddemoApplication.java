0311d4cdf555df573a958c9d5655e69f037d60eb
Agustin Bollati <bollatiagustin@gmail.com>
2026-02-23 16:26:46 -0300

    feat(CruddemoApplication): implementar creacion de estudiante

--- 03-hibernate-jpa-crud/01-curddemo-student/cruddemo/src/main/java/com/agustinbollati/cruddemo/CruddemoApplication.java
+++ 03-hibernate-jpa-crud/01-curddemo-student/cruddemo/src/main/java/com/agustinbollati/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.agustinbollati.cruddemo;
 
+import com.agustinbollati.cruddemo.dao.StudentDAO;
+import com.agustinbollati.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +17,23 @@ public class CruddemoApplication {
 	// Será ejecutado después de que el contexto de la aplicación se haya cargado
 	// y la aplicación esté lista para funcionar.
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// crear el objeto estudiante
+		System.out.println("Creating new student object...");
+		Student student = new Student("Agustin", "Bollati", "agubollati@miempresa.com.ar");
+
+		// guardar el estudiante en la base de datos
+		System.out.println("Saving the student...");
+		studentDAO.save(student);
+
+		// mostrar el id del estudiante guardado
+		System.out.println("Saved student. Generated id: " + student.getId());
 	}
 
 }
