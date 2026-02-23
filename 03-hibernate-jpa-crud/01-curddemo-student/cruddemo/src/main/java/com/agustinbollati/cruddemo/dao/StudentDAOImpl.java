ca8a1bd83765d42ab19c4d5814a9251eb4eb3dd3
Agustin Bollati <bollatiagustin@gmail.com>
2026-02-23 16:25:49 -0300

    feat(dao): implementar StudentDAOImpl con método save()

---
+++ 03-hibernate-jpa-crud/01-curddemo-student/cruddemo/src/main/java/com/agustinbollati/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,28 @@
+package com.agustinbollati.cruddemo.dao;
+
+import com.agustinbollati.cruddemo.entity.Student;
+
+import jakarta.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+	// Definir campo para el EntityManager
+	private final EntityManager entityManager;
+
+	@Autowired
+	// Inyectar el EntityManager usando constructor injection
+	public StudentDAOImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	// Implementar el método save() para guardar el estudiante en la base de datos
+	@Override
+	@Transactional
+	public void save(Student student) {
+		this.entityManager.persist(student);
+	}
+}
